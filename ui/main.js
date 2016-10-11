becc2270ac99105e374aa49f4a7ba8f9db9ffa06
siddharthc9 <siddharthmyworld007@gmail.com>
2016-10-11 21:07:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 //move the img
-var img = document.getElementById('madi');
+/*var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft=10;
@@ -11,4 +11,4 @@ function moveRight(){
 img.onclick = function(){
     var interval = setInterval(moveRight, 100);
   //   img.style.marginleft='100px';
-};
+};*/
