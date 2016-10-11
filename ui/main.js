4e232c1a2afe237fdf414e11176b697bd1e4a7c1
siddharthc9 <siddharthmyworld007@gmail.com>
2016-10-11 21:00:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,12 @@ var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 //move the img
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft=10;
+    img.style.marginLeft=marginLeft='px';
+}
 img.onclick = function(){
-    img.style.marginleft='100px';
+    var interval = setInterval(moveRight, 100);
+  //   img.style.marginleft='100px';
 };
